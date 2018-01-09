7e1ff9583d9a67b92b26a6bca36751e99b9828b0
webdevthierry <thierry.hor.sd@gmail.com>
2018-01-09 14:16:39 +0100

    build architecture of the app, add class character.js & day.js

---
+++ www/assets/js/class/character.js
@@ -0,0 +1,9 @@
+class Character {
+    constructor(name, health, strength, stamina, money) {
+        this.name = name;
+        this.health = health;
+        this.stamina = stamina;
+        this.money = money;
+        this.isAlive = true;
+    }
+}
---
+++ www/assets/js/class/day.js
@@ -0,0 +1,7 @@
+class Day {
+    constructor(actions, days, night) {
+        this.actions = actions;
+        this.days = days;
+        this.night = false;
+    }
+}
