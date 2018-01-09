3c2b56918218d1c42f32f1940e87131cb3a548bb
webdevthierry <thierry.hor.sd@gmail.com>
2018-01-09 16:46:05 +0100

    Add part1 of fighting system success

--- www/assets/js/class/character.js
+++ www/assets/js/class/character.js
@@ -1,10 +1,23 @@
 class Character {
-    constructor(name, health, strength, stamina, money) {
+    constructor(name, health, strength, stamina, money = 0) {
         this.name = name;
         this.health = health;
         this.strength = strength;
         this.stamina = stamina;
         this.money = money;
         this.isAlive = true;
+    }
+
+    attack(char2) {
+        char2.health -= this.strength;
+        if (char2.health <= 0) {
+            char2.health = 0;
+            char2.isDead();
+        }
+        return char2.health;
+    }
+
+    isDead() {
+        this.isAlive = false;
     }
 }
