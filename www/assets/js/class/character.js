d3faf3d660cbe0535794ca1d1d7df9266edd243e
webdevthierry <thierry.hor.sd@gmail.com>
2018-01-09 14:43:55 +0100

    add script.js & property to class character

--- www/assets/js/class/character.js
+++ www/assets/js/class/character.js
@@ -2,6 +2,7 @@ class Character {
     constructor(name, health, strength, stamina, money) {
         this.name = name;
         this.health = health;
+        this.strength = strength;
         this.stamina = stamina;
         this.money = money;
         this.isAlive = true;
