3c81e318208d0f0e2bfe4c41abd25ecbd4b41de4
webdevthierry <thierry.hor.sd@gmail.com>
2018-01-16 17:34:18 +0100

    add bubble

--- www/assets/js/script.js
+++ www/assets/js/script.js
@@ -1,10 +1,11 @@
 let startBtn = document.querySelector("#play");
 let startPage = document.querySelector("#start-page");
-
+let contentHouse = document.querySelector(".content");
 
 startBtn.addEventListener("click", function() {
     startPage.classList.add("remove-background");
     startPage.addEventListener("animationend", function() {
         startPage.style.display = "none";
+        contentHouse.style.display = "block";
     });
 });
