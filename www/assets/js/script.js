56dc095b34125cb502a1b65ebac6204c25dbc8c1
webdevthierry <thierry.hor.sd@gmail.com>
2018-01-18 15:31:27 +0100

    change kirby and bubble position, add event on keypress enter

--- www/assets/js/script.js
+++ www/assets/js/script.js
@@ -1,11 +1,21 @@
 let startBtn = document.querySelector("#play");
 let startPage = document.querySelector("#start-page");
 let contentHouse = document.querySelector(".content");
+let house = document.querySelector("#house");
 
-startBtn.addEventListener("click", function() {
+startBtn.addEventListener("click", function () {
     startPage.classList.add("remove-background");
-    startPage.addEventListener("animationend", function() {
+    startPage.addEventListener("animationend", function () {
         startPage.style.display = "none";
         contentHouse.style.display = "block";
     });
 });
+
+// while (house.style.display === "block") {
+//     document.addEventListener("keypress", function () {
+//         if (event.keyCode == 13) {
+//             console.log("enter");
+
+//         }
+//     });
+// }
