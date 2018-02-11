a7a14726a62a99601c85e62f28ed8abffeae9821
webdevthierry <thierry.hor.sd@gmail.com>
2018-02-11 15:56:54 +0100

    add music, compressed music, Whispy Woods sprites

--- www/assets/js/test.js
+++ www/assets/js/test.js
@@ -8,6 +8,10 @@ console.log(knight);
 let kirbyHealth = document.querySelector("#kirby-health");
 let knightHealth = document.querySelector("#knight-health");
 
+// Songs
+let kirbyWinSong = document.querySelector("#win-song");
+let kirbyLoseSong = document.querySelector("#game-over-song");
+
 let startBtn = document.querySelector("#play");
 // Start Section
 let startPage = document.querySelector("#start-page");
@@ -32,12 +36,16 @@ let contentArena = document.querySelector(".content-arena");
 // Fight Scene
 let metalKnightScene = document.querySelector(".metal-knight-scene");
 let metalKnightChar = document.querySelector("#metal-knight");
+let metalKnightSideFight = document.querySelector(".metal-knight-side-fight")
+
+let whispyWoodsChar = document.querySelector("#whispy-woods-scene");
 let fighters = document.querySelector(".fighters");
 let charSelect = document.querySelector(".char-select");
 let fightSceneContainer = document.querySelector(".fight-scene-container");
 let kirbyFight = document.querySelector(".kirby-fight");
-let knightFight = document.querySelector(".knight-fight");
+let metalKnightFight = document.querySelector(".metal-knight-fight");
 let gameOver = document.querySelector("#game-over");
+let kirbyWin = document.querySelector(".kirby-win");
 
 let num1 = document.querySelector(".num1");
 let num2 = document.querySelector(".num2");
@@ -50,53 +58,10 @@ let total = randomNum1 + randomNum2;
 num1.textContent = randomNum1;
 num2.textContent = randomNum2;
 
-metalKnightChar.addEventListener("click", function () {
-  kirby.doSth();
-  fightMode();
-  charSelect.classList.add("hide-section");
-  document.addEventListener("submit", function (event) {
-    event.preventDefault();
+metalKnightChar.addEventListener("click", fightWithKnight);
 
-    if (parseInt(answer.value) === total) {
-      newNumbers();
-      kirbyFight.classList.add("kirby-anime");
-      kirby.attack(knight);
-      knightHealth.value = knight.health;
 
-      document.addEventListener("animationend", function () {
-        kirbyFight.classList.remove("kirby-anime");
-      });
-    } else if (parseInt(answer.value) < total || parseInt(answer.value) > total) {
-      newNumbers();
-      knightFight.classList.add("knight-anime");
-      knight.attack(kirby);
-      kirbyHealth.value = kirby.health;
-
-      document.addEventListener("animationend", function () {
-        knightFight.classList.remove("knight-anime");
-      });
-    } else {
-      console.log("not a number");
-    }
-    if (!kirby.isAlive || !knight.isAlive) {
-      window.setTimeout(function() {
-        if (!kirby.isAlive) {
-          endFightMode();
-          charSelect.classList.add("hide-section");
-          gameOver.classList.remove("hide-section");
-        } else if (!knight.isAlive) {
-      window.setTimeout(function() {
-        
-      })
-          endFightMode();
-          console.log("knight is dead");
-        }
-      },2000);
-      
-    }
-  });
-});
-
+// FUNCTIONS
 function endFightMode() {
   charSelect.classList.remove("hide-section");
   fighters.classList.add("hide-section");
@@ -115,4 +80,70 @@ function newNumbers() {
   total = randomNum1 + randomNum2;
   num1.textContent = randomNum1;
   num2.textContent = randomNum2;
+}
+
+// Futur Edit
+/* 
+function resetStats(character) {
+  character.health = 100;
+  character.isAlive = true;
+  answer.value = "";
+} */
+
+// TEST
+
+function fightWithKnight() {
+  kirby.doSth();
+  fightMode();
+  charSelect.classList.add("hide-section");
+  document.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    if (parseInt(answer.value) === total) {
+      newNumbers();
+
+      kirbyFight.classList.add("kirby-anime");
+      kirby.attack(knight);
+
+      knightHealth.value = knight.health;
+      answer.value = "";
+
+      document.addEventListener("animationend", function () {
+        kirbyFight.classList.remove("kirby-anime");
+      });
+    } else if (parseInt(answer.value) < total || parseInt(answer.value) > total) {
+      newNumbers();
+      metalKnightFight.classList.add("knight-anime");
+      knight.attack(kirby);
+      kirbyHealth.value = kirby.health;
+      answer.value = "";
+
+      document.addEventListener("animationend", function () {
+        metalKnightFight.classList.remove("knight-anime");
+      });
+    } else {
+      console.log("not a number");
+    }
+    if (!kirby.isAlive || !knight.isAlive) {
+      window.setTimeout(function () {
+        if (!kirby.isAlive) {
+          endFightMode();
+          charSelect.classList.add("hide-section");
+          gameOver.classList.remove("hide-section");
+          kirbyLoseSong.play();
+        } else if (!knight.isAlive) {
+          window.setTimeout(function () {
+            endFightMode();
+            console.log("knight is dead");
+            kirbyWin.classList.add("hide-section");
+          }, 7000);
+          kirbyWin.classList.remove("hide-section");
+          kirbyWinSong.play();
+        }
+      }, 2000);
+
+    }
+  });
+
+ metalKnightChar.removeEventListener('click', fightWithKnight);
 }
